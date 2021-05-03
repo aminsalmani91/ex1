3f4b08c379c289df186856f7579d2028fbff01ea
aminsalmani91 <79279762+aminsalmani91@users.noreply.github.com>
2021-05-03 12:50:59 -0700

    Create prog3.py

---
+++ ex2/prog3.py
@@ -0,0 +1,22 @@
+def barresi(n):
+    
+    if n==1 or n<0:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        c=[]
+        for i in range(2,n):
+            if n%i==0:
+                c.append(i)
+        if len(c)==0:
+            return 1
+        else:
+            return 0
+
+print("exit=0")
+while True:
+    n=int(input("enter a number = "))
+    if n==0:
+        break
+    print(bool(barresi(n)))
